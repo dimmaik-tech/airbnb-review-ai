e1253efd758943570b6a547a7c98ca03078e5184
dimmaik-tech <dimmaik@gmail.com>
2026-02-18 16:26:56 +0200

    Update 2_History.py

--- pages/2_History.py
+++ pages/2_History.py
@@ -1,6 +1,10 @@
 from auth import require_login, show_logout_button
 require_login("Host Reply Pro")
 show_logout_button()
+
+from auth import require_login, show_logout_button
+require_login("Host Reply Pro")
+show_logout_button()
 import json
 import streamlit as st
 
